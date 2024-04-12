d3075f83b9814450cce7e8a71f4c3a59d9cdc691
Jorge Santana <166669976+santana-025@users.noreply.github.com>
2024-04-12 20:18:40 -0300

    Adicionando o ano

--- Dev Aprender/script.py
+++ Dev Aprender/script.py
@@ -1,3 +1,8 @@
 nome = str(input("Qual o seu nome: "))
+ano = int(input("Em que ano Nasceu :"))
 idade = int(input("Qual a sua idade :"))
+
+
 print("Olá", nome)
+print("Sua idade é", idade)
+print("Você nasceu em :", ano)
