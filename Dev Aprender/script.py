7ffb85c8835a905800a3fb57e653afcbd9708090
Jorge Santana <166669976+santana-025@users.noreply.github.com>
2024-04-12 21:01:10 -0300

    IF e else

--- Dev Aprender/script.py
+++ Dev Aprender/script.py
@@ -1,8 +1,10 @@
 nome = str(input("Qual o seu nome: "))
 ano = int(input("Em que ano Nasceu :"))
 idade = int(input("Qual a sua idade :"))
-
-
 print("Olá", nome)
 print("Sua idade é", idade)
 print("Você nasceu em :", ano)
+if nome == "Jorge":
+    print("Olá Jorge")
+else:
+    print("Crlh errado")
